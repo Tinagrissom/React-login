e7ce21569127d67330902ea787c0885f3199a45e
Tina <Tina@macusers-MBP-4.attlocal.net>
2021-06-24 17:57:25 -0500

    added useEffect hook to check local storage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedLogInInfo = localStorage.getItem("isLoggedIn");
+    // reaching out to local storage, call getItem and search for isLoggedIn info
+    //  returns info on isLoggedIn
+
+    if (storedLogInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  // call useEffect, pass 2 areguments
+  // 1st - anonomouse arrow function
+  // 2nd - array of dependancies, the functions will only run if the dependancies 
+  // change, and when the app first runs
+  // because we left the array empty, this will only run when we load the app
+  // this avoids an infinite loop
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false);
   };
 
